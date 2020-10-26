caa6bf765690a2ba4331702a5a63b6e9bf16255a
devchea <rchea817@gmail.com>
2020-10-25 22:01:04 -0700

    "updates"

--- test.js
+++ test.js
@@ -1,13 +1,29 @@
-const nums = [1, 2, 3];
-const showNums = (f1, f2, f3) => {
-  console.log(`show numbers: ${f1}, ${f2}, ${f3}`);
-};
-showNums(...nums); //show numbers: 1, 2, 3
-nums.pop();
-nums.pop();
-nums.push(4);
-nums.push(5);
-showNums(...nums); //show numbers: 1, 4, 5
-nums.pop();
-nums.pop();
-showNums(...nums, 6); //show numbers: 1, 6, undefined
+const clickHeels = (location) => {
+  return new Promise((resolve, reject) => {
+    console.log(`I want to go to ${location}`)
+    if (location === 'home') {
+      resolve('Home is correct, let\'s go home')
+    } else {
+      reject('Sorry, we can only go home')
+    }
+  })
+}
+
+const processRequest = (response) => {
+  return new Promise((resolve, reject) => {
+    console.log('Processing response')
+    resolve(`Extra information + ${response}`)
+  })
+}
+
+clickHeels('home')
+  .then(response => {
+    console.log('Response Received')
+    return processRequest(response)
+  })
+  .then(processResponse => {
+    console.log(processResponse)
+  })
+  .catch(err => {
+    console.log(err);
+  })
