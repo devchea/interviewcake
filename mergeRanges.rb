885a5e0f305fb267718327995e94cc64a28f2579
devchea <rchea817@gmail.com>
2020-12-18 22:32:21 -0800

    "updates"

--- mergeRanges.rb
+++ mergeRanges.rb
@@ -5,7 +5,7 @@ def merge_ranges(meetings)
   # Initialize merged_meetings with the earliest meeting.
   merged_meetings = [sorted_meetings[0]]
 
-  sorted_meetings[1].each do ||     
+  sorted_meetings[1].each do ||      
 
   # sorted_meetings[1..-1].each do |current_meeting_start, current_meeting_end|
   #   last_merged_meeting_start, last_merged_meeting_end = merged_meetings[-1]
