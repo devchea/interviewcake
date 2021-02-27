699eb86d8828fdb9650bb703e49cd4e0c3829a41
devchea <rchea817@gmail.com>
2021-02-26 16:03:08 -0800

    "updates"

--- HackerRank/WarmUpChallenges/salesByMatch.js
+++ HackerRank/WarmUpChallenges/salesByMatch.js
@@ -1,7 +1,24 @@
 const n = 9
-const ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+const arr = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 //ans = 9
 
-salesByMatch = (n, ar) => {
-    console.log(n);
+salesByMatch = (n, arr) => {
+    let count = 0
+    const map = new Map()
+
+    for (let i = 0; i < arr.length; i++) {
+        const num = arr[i];
+
+        if (map.has(num)) {
+            count++
+            map.delete(num)
+        } else {
+            map.set(num, 1)
+        }
+        
+    }
+    console.log(count);
+    console.log(map);
 }
+
+salesByMatch(n, arr)
