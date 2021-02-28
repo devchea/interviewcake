dc9557df750b1345ad234484288d81f1624bdd17
devchea <rchea817@gmail.com>
2021-02-28 15:38:53 -0800

    "updates"

--- HackerRank/WarmUpChallenges/countingValleys.js
+++ HackerRank/WarmUpChallenges/countingValleys.js
@@ -1,9 +1,29 @@
 const countingValleys = (steps, path) => {
-    console.log(path);
+    let numOfValleys = 0;
+    const pathArr = path[0].split('')
+
     if (steps >= 2 && steps <= 10^6) {
+        let curStep = 0
+        let i = 0
+        while (i <= steps) {
+            const seaLevel = 0
 
+            if (pathArr[i] == 'U') {
+                curStep ++
+                if (curStep == 0) {
+                    numOfValleys++
+                }
+                console.log('up',curStep);
+            } else if (pathArr[i] == 'D') {
+                curStep--
+                console.log('down', curStep);
+            }
+
+            i++
+        }
     }
-
+    
+    return numOfValleys
 }
 
-countingValleys( steps = 8, path = [UDDDUDUU])
+countingValleys( steps = 12, path = ['DDUUDDUDUUUD'])
